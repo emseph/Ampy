708069d9a5a8fe9ba2c0c00b31cee5d4691a2a37
Emmanuel Joseph Ellaga <emmanuel-joseph.ellaga@atos.net>
2022-10-09 12:26:58 +0800

    Update main.py
    - Added move function
    - Initalized blank buttons

--- main.py
+++ main.py
@@ -27,8 +27,10 @@ yLoc = int(winY - winy)
 print('Window location at: ' + str(xLoc) + ' ' + str(yLoc))
 
 # Window Components START
-btn = Button(window, text="Click Me")
-btn.grid(column=1, row=0)
+btnStart = Button(window, text="Start")
+btn.grid(column=0, row=0)
+btnEnd = Button(window, text="End")
+btn.grid(column=0, row=1)
 # Window Components END
 
 window.eval('tk::PlaceWindow . center')
@@ -37,11 +39,10 @@ window.geometry("{}x{}+{}+{}".format(winW,winH,xLoc,yLoc))
 window.mainloop()
 
 
-"""
-while(autoloop == 0):
-    for z in range(1,2):
-        x = random.randint(0,500)
-        y = random.randint(0,500)
-        pyautogui.moveTo(x,y)
-        time.sleep(2)
-"""
+def move():
+    while(autoloop == 0):
+        for z in range(1,2):
+            x = random.randint(0,500)
+            y = random.randint(0,500)
+            pyautogui.moveTo(x,y)
+            time.sleep(2)
